70d4e07f9338a88a3bf30e2a8699270a412a7623
Mira <91933198+miramarth@users.noreply.github.com>
2023-03-22 11:07:27 +0700

    Add files via upload

---
+++ src/tokobuku/TokoBuku.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tokobuku;
+
+/**
+ *
+ * @author User
+ */
+public class TokoBuku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formbuku data = new Formbuku();
+        data.setVisible(true);
+    }
+    
+}
